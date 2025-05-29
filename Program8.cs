31e22a8c6a8aba4c04c4ebe5bbdcf6b9678b501d
Max <m.kkataev@mail.ru>
2025-05-29 19:40:52 +0500

    IEnumerable

---
+++ Program8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new[] { 1, 2, 3, 4, 5, 6 };
+            var query = arr
+                .Select(el =>
+                {
+                    Console.WriteLine(el);
+                    return el;
+                })
+                .Where(el => el > 4)
+                .Select(el =>
+                {
+                    var res = el * 2;
+                    Console.WriteLine(res);
+                    return res;
+                });
+            Console.WriteLine("Output");
+            _ = query.ToArray();
+        }
+    }
+}
